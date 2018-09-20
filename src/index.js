a187d16d48fa21cc58d01d36d1381037c5a2e5cd
Crypto-kittensSeller <Nikolay.Shaveiko@gmail.com>
2018-09-20 12:55:59 +0300

    Quadratic-Equation calculation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let solution = [];
+  let arr = equation.split(" ");
+  let a = arr[0];
+  let b = arr[3]+arr[4];
+  let c = arr[7]+arr[8];
+  let discr = b*b-4*a*c;
+  let x1 = Math.round((-b+Math.sqrt(discr))/(2*a));
+  let x2 = Math.round((-b-Math.sqrt(discr))/(2*a));
+  solution.push(x2);
+  solution.push(x1);
+  solution.sort(function(a,b){return a - b});
+  return solution;
 }
